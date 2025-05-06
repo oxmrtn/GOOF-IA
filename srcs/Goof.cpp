7ff812a3fd71a4755efadc834428ebae63a123e3
Ismaël <ismabrf@outlook.fr>
2025-05-06 14:57:04 +0200

    modif goof.cpp

--- srcs/Goof.cpp
+++ srcs/Goof.cpp
@@ -5,8 +5,16 @@ Goof::Goof(Game initstate)
     this->currentState = initState;
 }
 
+Goof::~Goof()
+{}
+
 void Goof::updateState(Game state)
 {
     this->currentState = state;
+}
+
+void setActionList()
+{
+
 }
 
